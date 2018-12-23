c0d568fb32b415b625f8a149d9d95b4367d24580
yriaznov <yoni_riaznov@bmc.com>
2018-12-23 22:32:06 +0200

    ignore js and map files

--- WebClient/src/app/app.main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_module_1 = require("./modules/app.module");
-angular.bootstrap(document, [
-    'ui.router',
-    app_module_1.appModule.name
-]);
-//# sourceMappingURL=app.main.js.map
